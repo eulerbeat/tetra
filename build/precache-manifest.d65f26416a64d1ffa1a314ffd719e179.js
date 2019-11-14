b834cb7d604fb61760f21e984fa81e26b5afb418
shark <roberto.gil.up@gmail.com>
2019-11-14 07:03:31 -0800

    build production

---
+++ build/precache-manifest.d65f26416a64d1ffa1a314ffd719e179.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6037a9053c57025664e32f4a6f15e519",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b87e6455ff3e3854dd48",
+    "url": "./static/css/main.66743ab8.chunk.css"
+  },
+  {
+    "revision": "b87e6455ff3e3854dd48",
+    "url": "./static/js/main.384663bc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "0a003fa669a49a921d442d3238edd2fc",
+    "url": "./static/media/logo.0a003fa6.png"
+  },
+  {
+    "revision": "4dc7de3347f48884a7fb27e0ba6b8f6b",
+    "url": "./static/media/vt323-latin-400.4dc7de33.woff2"
+  },
+  {
+    "revision": "d037f8cb75b5764ae757659ae551cc62",
+    "url": "./static/media/vt323-latin-400.d037f8cb.woff"
+  }
+]);
